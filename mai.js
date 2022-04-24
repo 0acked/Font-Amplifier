3923ad2ba0666fc5a8770f044619130e93684a3e
0acked <98188838+0acked@users.noreply.github.com>
2022-04-24 13:16:37 +0100

    Add files via upload

--- mai.js
+++ mai.js
@@ -8,13 +8,16 @@ function setup(){
     video.size(550, 500);
 
     canvas = createCanvas(550, 500);
-    canvas.position(750, 275);
+    canvas.position(750, 230);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose",gotPoses);
 }
 function draw (){
     background("#7FFFD4");
+    textSize(difference);
+    fill("#32CD32");
+    text("AKO", 50, 200);
 }
 function modelLoaded(){
     console.log("PoseNet has been integrated!");
@@ -23,6 +26,10 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
-        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference =floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = "+leftWristX+", rightWristX = "+rightWristX+" and the difference is "+difference);
     }
 }
